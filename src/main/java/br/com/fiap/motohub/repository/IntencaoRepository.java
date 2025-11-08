f4e160374ec3059b803af56e7c7a31f32fe4e35d
AssisCarol <assiscaroline14@icloud.com>
2025-11-08 16:29:46 -0300

    Adiciona testes unitários, melhorias no ClienteService e estrutura final da Sprint

---
+++ src/main/java/br/com/fiap/motohub/repository/IntencaoRepository.java
@@ -0,0 +1,8 @@
+package br.com.fiap.motohub.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.fiap.motohub.model.Intencao;
+
+public interface IntencaoRepository extends JpaRepository<Intencao, Long> {
+}
